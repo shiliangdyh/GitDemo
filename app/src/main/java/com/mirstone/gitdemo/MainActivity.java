6b84b8d2c743837a37449c01248373a4b9fa3bef
shiliang-hz <mirstone@126.com>
2019-05-29 09:48:12 +0800

    添加

--- app/src/main/java/com/mirstone/gitdemo/MainActivity.java
+++ app/src/main/java/com/mirstone/gitdemo/MainActivity.java
@@ -2,12 +2,19 @@ package com.mirstone.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+    }
+    private void toast(){
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/mirstone/gitdemo/Person.java
@@ -0,0 +1,4 @@
+package com.mirstone.gitdemo;
+
+public class Person {
+}
