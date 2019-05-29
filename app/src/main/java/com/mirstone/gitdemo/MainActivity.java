34ebf5c91c538f735de595e2d80076b2767ede5c
shiliang-hz <mirstone@126.com>
2019-05-29 10:01:27 +0800

    aaa

--- app/src/main/java/com/mirstone/gitdemo/MainActivity.java
+++ app/src/main/java/com/mirstone/gitdemo/MainActivity.java
@@ -2,12 +2,21 @@ package com.mirstone.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+            }
+        });
     }
 }
